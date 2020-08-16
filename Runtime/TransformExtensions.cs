16df48074eb9b8055819ee7eb96279913a92eaa5
Liam Knightley <liam.knightley@gmail.com>
2020-08-15 21:29:45 -0700

    Added transform extensions

---
+++ Runtime/TransformExtensions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Extensions.Runtime
+{
+    public static class TransformExtensions
+    {
+        public static void DestroyAllChildren(this Transform transform)
+        {
+            for (var t = transform.childCount - 1; t >= 0; t--)
+            {
+                if (Application.isPlaying)
+                {
+                    Object.Destroy(transform.GetChild(t).gameObject);
+                }
+                else
+                {
+                    Object.DestroyImmediate(transform.GetChild(t).gameObject);
+                }
+            }
+        }
+    }
+}
